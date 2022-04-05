6b876a934adc621bca61ed75620ec9afb5bfaaad
itsjessf <jessica.pferreira@outlook.com>
2022-04-05 14:55:45 +0200

    Finished section 7

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <li>
+      <h2>
+        {{ friend.name }}
+      </h2>
+      <button @click="toggleDetails">
+        {{ detailsAreVisible ? "Hide" : "Show" }} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone</strong> {{ friend.phone }}</li>
+        <li><strong>Email</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Pereira",
+        phone: "364758",
+        email: "manuel@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
