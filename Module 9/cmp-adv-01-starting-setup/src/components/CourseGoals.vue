2a664c37e0e808cf1f32b023c39659c73b8cfb06
itsjessf <jessica.pferreira@outlook.com>
2022-04-06 18:36:46 +0200

    Module 9 Done

---
+++ Module 9/cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal"> {{goal}}</li>
+    <slot :item="goal"></slot>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["Finishe the course", "Learn vue"],
+    };
+  },
+};
+</script>
---
+++ Module 9/cmp-adv-01-starting-setup/src/components/ErrorAlert.vue
@@ -0,0 +1,16 @@
+<template>
+     <dialog open>
+          <slot></slot>
+     </dialog>
+</template>
+
+<style scoped>
+dialog {
+     position: fixed;
+     top: 20vh;
+     left: 30%;
+     width: 30%;
+     background-color: white;
+     box-shadow: black;
+}
+</style>
